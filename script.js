06cdf6fcdf22f05f321fbccf580291a92fae9416
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-04-20 12:59:50 +0200

    Now modal also closes when clicked outside of it

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const addBookButton = document.querySelector("#add_book_btn");
 const modal = document.querySelector("dialog");
 const closeForm = document.querySelector("#cancel_form");
 
+modal.addEventListener("click", (e) =>
+{
+	const dialogDimensions = modal.getBoundingClientRect();
+	if (
+		e.clientX < dialogDimensions.left ||
+		e.clientX > dialogDimensions.right ||
+		e.clientY < dialogDimensions.top ||
+		e.clientY > dialogDimensions.bottom
+	)
+		modal.close();
+});
+
 addBookButton.addEventListener("click", () =>
 {
 	modal.showModal();
